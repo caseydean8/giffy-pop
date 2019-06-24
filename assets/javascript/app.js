a64f588d1000591ceeb2192d358282375628957b
Casey Carroll <caseydean8@gmail.com>
2019-06-24 10:50:12 -0400

    Added for-loop to populate buttons from array.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,16 @@
 // Create an array.
-var buttonArr = [dog, cat, pig];
+var buttonArr = ["dog", "cat", "pig"];
 
 // 2. Create buttons to represent array items
+document.onload = buttonCreate();
+function buttonCreate(){
+    for (i = 0; i < buttonArr.length; i++){
+        var gifButton = document.createElement("button");
+        gifButton.classList.add("btn");
+        gifButton.innerHTML = buttonArr[i];
+        $("#button-holder").append(gifButton);
+    }
+}
 
 
 // 3. Buttons retrieve 10 related gifs on press.
