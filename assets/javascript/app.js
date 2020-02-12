27487b41887432290d988bc186595387e5000384
Casey Carroll <caseydean8@gmail.com>
2020-02-11 20:20:20 -0500

    Created separate functions
    Moved functions for ajax call and img div creations out of click event.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
-
 // Create buttons to represent array items
-const buttonCreate = (item) => {
-  console.log(item);
+const buttonCreate = item => {
   document.getElementById("button-holder").innerHTML = "";
   let topics = ["mouse", "cat", "piggy"];
   if (item) topics.push(item);
@@ -20,19 +18,25 @@ window.onload = buttonCreate();
 $("#add-button").on("click", function(event) {
   event.preventDefault();
   // Disable submit button if input field is blank.
-  if ($("#user-input").val().length != 0) {
+  if ($("#user-input").val()) {
     this.disabled = false;
-    // let userChoice = ""
     const newButton = $("#user-input")
       .val()
       .trim();
-    // topics.push(newButton);
     $("#user-input").val("");
     buttonCreate(newButton);
     gifSearch(newButton);
   }
 });
 
+// Buttons retrieve 10 related still images/gifs.
+
+$(document).on("click", ".btn", function(event) {
+  event.preventDefault();
+  gifSearch(this.textContent);
+});
+
+// Search via giphy.com api
 const gifSearch = searchTerm => {
   // Clear any previous gifs.
   document.getElementById("gif-holder").innerHTML = "";
@@ -47,54 +51,51 @@ const gifSearch = searchTerm => {
   }).then(function(response) {
     const apiResults = response.data;
     // console.log(apiResults);
-    apiResults.forEach(result => {
-      if (result.rating !== "r" && result.rating !== "pg-13") {
-        const imageNest = $("<div>").addClass("gifDiv");
-        const rating = result.rating;
-
-        const ratingSpan = $("<p>").text("Rated " + rating);
-
-        // Creating and storing an image tag
-        const gifImageTag = $("<img>");
-
-        // Adding a class and setting the gifImageTag src attribute to imageUrl
-        gifImageTag.attr({
-          class: "gif-style",
-          alt: `${searchTerm} image`
-        });
-
-        gifImageTag.attr("src", result.images.fixed_width_still.url);
-
-        gifImageTag.attr("data-still", result.images.fixed_width_still.url);
-
-        gifImageTag.attr("data-playing", result.images.fixed_width.url);
-
-        gifImageTag.attr("data-state", "still");
-
-        // Append paragraph span and gif to imageNest
-        imageNest.append(ratingSpan);
-        imageNest.append(gifImageTag);
-
-        // Append at gif-holder div
-        $("#gif-holder").append(imageNest);
-      }
-    });
+    gifDisplay(apiResults);
   });
 };
 
-// Buttons retrieve 10 related still images/gifs.
-
-$(document).on("click", ".btn", function(event) {
-  event.preventDefault();
-  gifSearch(this.textContent);
-});
-
 // 4. Display still gifs.
+const gifDisplay = array => {
+  array.forEach(result => {
+    if (result.rating !== "r" && result.rating !== "pg-13") {
+      // console.log(result);
+      const imageNest = $("<div>").addClass("gifDiv");
+      const rating = result.rating;
+
+      const ratingSpan = $("<p>").text("Rated " + rating);
+
+      // Creating and storing an image tag
+      const gifImageTag = $("<img>");
+
+      // Adding a class and setting the gifImageTag src attribute to imageUrl
+      gifImageTag.attr({
+        class: "gif-style",
+        alt: `${result.title}`
+      });
+
+      gifImageTag.attr("src", result.images.fixed_width_still.url);
+
+      gifImageTag.attr("data-still", result.images.fixed_width_still.url);
+
+      gifImageTag.attr("data-playing", result.images.fixed_width.url);
+
+      gifImageTag.attr("data-state", "still");
+
+      // Append paragraph span and gif to imageNest
+      imageNest.append(ratingSpan);
+      imageNest.append(gifImageTag);
+
+      // Append at gif-holder div
+      $("#gif-holder").append(imageNest);
+    }
+  });
+};
 
 // 5. Gif starts upon user click.
 
 $(document).on("click", "img.gif-style", function() {
-  var state = $(this).attr("data-state");
+  const state = $(this).attr("data-state");
 
   // If clicked image's state is still, change its src to what its data-playing value is.
   if (state === "still") {
